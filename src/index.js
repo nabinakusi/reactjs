76e62945b1c5b1125daeb2cd8509b5df502e1108
nabinakusi <navnakusi19@gmail.com>
2021-05-14 14:44:49 +0545

    first push

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+//* component named app
+const App = () => {
+    return (
+        <div>
+            Hi there!
+        </div>
+    )
+}
+
+
+//* renderer
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
+
+
